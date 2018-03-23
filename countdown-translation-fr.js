b5c0b4a332520c6090b3b8bcb22a2eb6d9cd17a5
florentroques <florent.open@gmail.com>
2018-03-23 13:44:53 +0200

    Create countdown-translation-fr.js

---
+++ countdown-translation-fr.js
@@ -0,0 +1,7 @@
+#requires jQuery
+$(document).ready(function() {
+  $(".time-name:eq(0)").text("jours");
+  $(".time-name:eq(1)").text("heures");
+  $(".time-name:eq(2)").text("minutes");
+  $(".time-name:eq(3)").text("secondes");
+});
